089ca18a48c1dc024693d24bce82b01360aa3274
quantumqat <58191215+quantumqat@users.noreply.github.com>
2021-10-24 20:49:01 -0500

    adding connection to data tables

--- peerreviewproject/StudentMain.aspx.cs
+++ peerreviewproject/StudentMain.aspx.cs
@@ -4,17 +4,34 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace peerreviewproject
 {
     public partial class StudentMain : System.Web.UI.Page
     {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=C:\USERS\SHAI1\PEER_REVIEW.MDF;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         protected void Page_Load(object sender, EventArgs e)
         {
-            lblStudentDetails.Text = "User ID: " + Session["email"];
+            using (SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                sqlCon.Open();
+                SqlDataAdapter sqlDa = new SqlDataAdapter("SELECT CourseDepartment, CourseNumber, CourseName FROM Course_table", sqlCon);
+                DataTable dt = new DataTable();
+                sqlDa.Fill(dt);
+                GridView1.DataSource = dt;
+                GridView1.DataBind();
+
+            }
+
+                lblStudentDetails.Text = "User ID: " + Session["email"];
             
             
         }
+
+       
 
         protected void logoutButton_Click(object sender, EventArgs e)
         {
